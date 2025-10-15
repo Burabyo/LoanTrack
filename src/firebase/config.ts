2dab981dd29e0f69d16f0108e0208545894a69cd
Burabyo Gift <burabyogift1@gmail.com>
2025-10-15 07:32:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6651766635-36540",
+  "appId": "1:322107442023:web:1274d87bde591dec1065c2",
+  "apiKey": "AIzaSyCtHs4dS347l1YcapFgP0-DpLiSGA3tjFM",
+  "authDomain": "studio-6651766635-36540.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "322107442023"
+};
