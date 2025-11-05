40cd920784f16bd6417a147b62f3c669f15ef580
Burabyo <j.burabyo@alustudent.com>
2025-11-05 14:09:12 +0200

    Enable reactStrictMode and update comments

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    unoptimized: true, // optional
+    unoptimized: true // required if using Firebase Storage images
   },
   experimental: {
-    optimizePackageImports: ['@radix-ui/react-*'],
-  },
+    optimizePackageImports: ['@radix-ui/react-*']
+  }
 };
 
 module.exports = nextConfig;
-
